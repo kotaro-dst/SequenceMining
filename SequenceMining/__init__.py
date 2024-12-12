28be22f66e85eeebe0db900c0daf2c2434d61243
kotaro-dst <kotaro.hirayama@adk.jp>
2024-12-12 16:28:56 +0900

    test1

---
+++ SequenceMining/__init__.py
@@ -0,0 +1,2 @@
+from SequenceMining.process_data import *
+from SequenceMining.run_analysis import *
---
+++ setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='SequenceMining',
+    version='0.1',
+    packages=find_packages()
+)
